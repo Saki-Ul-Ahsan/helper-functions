2b230cb92c7102c93e3635bf64caaeb1fe518625
Saki-Ul-Ahsan <sakib.ahsan.35@gmail.com>
2025-09-22 12:24:44 +0600

    comma separated values

---
+++ comma-separated-amounts.ts
@@ -0,0 +1,6 @@
+export function formatCurrencyCommaSeparated(amount: number): string {
+  return new Intl.NumberFormat("en-US", {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(amount);
+}
